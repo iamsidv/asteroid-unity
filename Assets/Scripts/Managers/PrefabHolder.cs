8172760915632dcf9011102ecc490d441b3407ed
Siddharth Verma <siddharthverma_911@yahoo.co.in>
2023-06-07 16:32:08 +0530

    Moved prefab references from PrefabHolder to GameConfig asset, namespace added.

--- Assets/Scripts/Managers/PrefabHolder.cs
+++ Assets/Scripts/Managers/PrefabHolder.cs
@@ -1,24 +1,17 @@
+using Asteroids.Game.Config;
 using Asteroids.Game.Core;
-using System.Collections;
 using System.Linq;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PrefabHolder : MonoBehaviour
 {
     public static PrefabHolder instance;
 
-    public GameplayElement[] gameplayElements;
-    public GameEntity playerBullet;
-    public GameEntity enemyBullet;
+    [SerializeField] private GameConfig config;
 
-    public Wave[] waves;
-
-    public int currentWave;
-    public int waveEnemiesCount;
-    public float timeStep;
-
-    public GameObject playerShip;
+    private int _currentWave;
+    private int _waveEnemiesCount;
+    private float _timeStep;
 
     private void Awake()
     {
@@ -30,55 +23,54 @@ public class PrefabHolder : MonoBehaviour
         if (MainManager.CurrentGameState != GameState.Running)
             return;
 
-        var wave = waves[currentWave];
-        if (Time.time - timeStep > wave.delay)
+        var wave = config.EnemyWaves[_currentWave];
+        if (Time.time - _timeStep > wave.Delay)
         {
-            if (waveEnemiesCount > wave.count)
+            if (_waveEnemiesCount > wave.Count)
             {
-                waveEnemiesCount = 0;
-                currentWave++;
+                _waveEnemiesCount = 0;
+                _currentWave++;
 
-                if (currentWave > waves.Length - 1)
-                    currentWave = 0;
+                if (_currentWave > config.EnemyWaves.Length - 1)
+                    _currentWave = 0;
 
-                timeStep = Time.time + 10f;
+                _timeStep = Time.time + 10f;
             }
-
 
             var degrees = Random.Range(0, 360f) * Mathf.Deg2Rad;
             var radius = Random.Range(10f, 15f);
             var pos = new Vector2(Mathf.Cos(degrees) * radius, Mathf.Sin(degrees) * radius);
 
-            var entityId = wave.enemies[Random.Range(0, wave.enemies.Length)];
+            var entityId = wave.Enemies[Random.Range(0, wave.Enemies.Length)];
             InstantiateEntity(entityId, pos);
 
-            waveEnemiesCount += 1;
+            _waveEnemiesCount += 1;
 
-            timeStep = Time.time;
+            _timeStep = Time.time;
         }
     }
 
     public IGameEntity InstantiatePlayerBullet(Vector2 position)
     {
-        return InstantiateEntity(playerBullet, position);
+        return InstantiateEntity(config.PlayerProjectile, position);
     }
 
     public IGameEntity InstantiateEnemyBullet(Vector3 position)
     {
-        return InstantiateEntity(enemyBullet, position);
+        return InstantiateEntity(config.EnemyProjectile, position);
     }
 
     public IGameEntity InstantiateEntity(string entityId, Vector2 position)
     {
-        var gameplayElement = gameplayElements.First(t => t.id.Equals(entityId));
-        var entity = InstantiateEntity(gameplayElement.prefab, position);
-        entity.OnInitialize(gameplayElement.score);
+        var gameplayElement = config.GameElements.First(t => t.Id.Equals(entityId));
+        var entity = InstantiateEntity(gameplayElement.Prefab, position);
+        entity.OnInitialize(gameplayElement.Score);
         return entity;
     }
 
     public void SpawnPlayerShip()
     {
-        Instantiate(playerShip, Vector3.zero, Quaternion.identity);
+        Instantiate(config.PlayerShip, Vector3.zero, Quaternion.identity);
     }
 
     public IGameEntity InstantiateEntity(IGameEntity entity, Vector2 position)

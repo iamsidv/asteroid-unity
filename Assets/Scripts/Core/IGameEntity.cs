d0f31af1b8fc938c117ccc2b75fe0c61a0ede653
Siddharth Verma <siddharthverma_911@yahoo.co.in>
2023-06-07 14:25:22 +0530

    Interfaces removed

--- Assets/Scripts/Core/IGameEntity.cs
+++ Assets/Scripts/Core/IGameEntity.cs
@@ -2,40 +2,20 @@ namespace Asteroids.Game.Core
 {
     public interface IGameEntity
     {
-        void SetDirection(UnityEngine.Vector2 direction);
-
-        void OnInitialize(int score);
-
-        void EntityStart();
-
-        void EntityUpdate();
-
-        void EntityFixedUpdate();
-
-        void DisposeEntity();
-
-        void SetVisibility(bool isVisible);
-
         UnityEngine.GameObject GameObject { get; }
-    }
 
-    public interface IGame
-    {
-        void AddGameEntity(IGameEntity gameEntity);
-
-        void RemoveGameEntity(IGameEntity gameEntity);
-
-        void OnStateChanged(IGameState state);
-
-        void UpdateGame();
-
-        void OnFixedUpdate();
+        void SetDirection(UnityEngine.Vector2 direction);
+        void OnInitialize(int score);
+        void EntityStart();
+        void EntityUpdate();
+        void EntityFixedUpdate();
+        void DisposeEntity();
+        void SetVisibility(bool isVisible);
     }
 
     public interface IGameState
     {
         void Permit();
-
         void Execute();
     }
 }

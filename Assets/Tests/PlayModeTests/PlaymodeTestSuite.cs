c08cec1ed4a3b65930563ecb957c06d47357d00c
Siddharth Verma <>
2025-04-17 11:36:54 +0300

    Updated bindings in the tests as they failed whenever new bindings gets introduced

--- Assets/Tests/PlayModeTests/PlaymodeTestSuite.cs
+++ Assets/Tests/PlayModeTests/PlaymodeTestSuite.cs
@@ -7,127 +7,138 @@ using Game.Engine.Entities;
 using Game.Managers;
 using Game.PlayerState;
 using Game.Signals;
+using Game.StateManagement;
+using Game.UI;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.TestTools;
 using Zenject;
 
-public class PlaymodeTestSuite : ZenjectIntegrationTestFixture
+namespace Tests.PlayModeTests
 {
-    [UnityTest]
-    public IEnumerator UT_PlayModeLoadAddressableTest()
+    public class PlaymodeTestSuite : ZenjectIntegrationTestFixture
     {
-        PreInstall();
-        PostInstall();
-
-        var operation = Addressables.LoadAssetAsync<GameConfig>("cfg_gameConfig.asset");
-
-        GameConfig isComplete = null;
-
-        operation.Completed += (handle) => { isComplete = handle.Result; };
-
-        while (isComplete == null)
+        [UnityTest]
+        public IEnumerator UT_PlayModeLoadAddressableTest()
         {
-            yield return null;
+            PreInstall();
+            PostInstall();
+
+            AsyncOperationHandle<GameConfig> operation = Addressables.LoadAssetAsync<GameConfig>("Assets/Config/GameConfig.asset");
+
+            Assert.That(operation.IsValid);
+
+            GameConfig isComplete = null;
+
+            operation.Completed += (handle) => { isComplete = handle.Result; };
+
+            while (isComplete == null)
+            {
+                yield return null;
+            }
+
+            Assert.IsNotNull(isComplete);
         }
 
-        Assert.IsNotNull(isComplete);
-    }
+        [UnityTest]
+        public IEnumerator UT_TestDeductPlayerLifeOnAsteriodCollision()
+        {
+            GameConfig gameConfig = AssetDatabase.LoadAssetAtPath<GameConfig>("Assets/Config/GameConfig.asset");
 
-    [UnityTest]
-    public IEnumerator UT_TestDeductPlayerLifeOnAsteriodCollision()
-    {
-        var gameConfig = AssetDatabase.LoadAssetAtPath<GameConfig>("Assets/Config/GameConfig.asset");
+            CommonPlayerShipInstallBindings<Asteroid>(gameConfig, gameConfig.GameElements[0].Prefab.gameObject);
 
-        CommonPlayerShipInstallBindings<Asteroid>(gameConfig, gameConfig.GameElements[0].Prefab.gameObject);
+            PlayerProfileService profileService = Container.Resolve<PlayerProfileService>();
+            profileService.SetTotalLives(3);
+            int totallives = profileService.GetTotalLives();
 
-        var profileService = Container.Resolve<PlayerProfileService>();
-        profileService.SetTotalLives(3);
-        var totallives = profileService.GetTotalLives();
+            PlayerShip ship = Container.Resolve<PlayerShip>();
+            Asteroid asteroid = Container.Resolve<Asteroid>();
+            asteroid.SetVisibility(true);
 
-        var ship = Container.Resolve<PlayerShip>();
-        var asteroid = Container.Resolve<Asteroid>();
-        asteroid.SetVisibility(true);
+            ship.transform.position = asteroid.transform.position = new Vector3(0, 0, 0);
 
-        ship.transform.position = asteroid.transform.position = new Vector3(0, 0, 0);
+            yield return null;
 
-        yield return null;
+            Assert.Less(profileService.GetTotalLives(), totallives);
+        }
 
-        Assert.Less(profileService.GetTotalLives(), totallives);
-    }
+        [UnityTest]
+        public IEnumerator UT_TestDeductPlayerLifeOnSaucerCollision()
+        {
+            GameConfig gameConfig = AssetDatabase.LoadAssetAtPath<GameConfig>("Assets/Config/GameConfig.asset");
 
-    [UnityTest]
-    public IEnumerator UT_TestDeductPlayerLifeOnSaucerCollision()
-    {
-        var gameConfig = AssetDatabase.LoadAssetAtPath<GameConfig>("Assets/Config/GameConfig.asset");
+            CommonPlayerShipInstallBindings<EnemySaucer>(gameConfig, gameConfig.GameElements[3].Prefab.gameObject);
 
-        CommonPlayerShipInstallBindings<EnemySaucer>(gameConfig, gameConfig.GameElements[3].Prefab.gameObject);
+            PlayerProfileService profileService = Container.Resolve<PlayerProfileService>();
+            profileService.SetTotalLives(3);
+            int totallives = profileService.GetTotalLives();
 
-        var profileService = Container.Resolve<PlayerProfileService>();
-        profileService.SetTotalLives(3);
-        var totallives = profileService.GetTotalLives();
+            PlayerShip ship = Container.Resolve<PlayerShip>();
+            EnemySaucer saucer = Container.Resolve<EnemySaucer>();
+            saucer.SetVisibility(true);
 
-        var ship = Container.Resolve<PlayerShip>();
-        var saucer = Container.Resolve<EnemySaucer>();
-        saucer.SetVisibility(true);
+            ship.transform.position = saucer.transform.position = new Vector3(0, 0, 0);
 
-        ship.transform.position = saucer.transform.position = new Vector3(0, 0, 0);
+            yield return null;
 
-        yield return null;
+            Assert.Less(profileService.GetTotalLives(), totallives);
+        }
 
-        Assert.Less(profileService.GetTotalLives(), totallives);
-    }
+        [UnityTest]
+        public IEnumerator UT_TestDeductPlayerLifeOnEnemyProjectileCollision()
+        {
+            GameConfig gameConfig = AssetDatabase.LoadAssetAtPath<GameConfig>("Assets/Config/GameConfig.asset");
 
-    [UnityTest]
-    public IEnumerator UT_TestDeductPlayerLifeOnEnemyProjectileCollision()
-    {
-        var gameConfig = AssetDatabase.LoadAssetAtPath<GameConfig>("Assets/Config/GameConfig.asset");
+            CommonPlayerShipInstallBindings<Bullet>(gameConfig, gameConfig.EnemyProjectile.gameObject);
 
-        CommonPlayerShipInstallBindings<Bullet>(gameConfig, gameConfig.EnemyProjectile.gameObject);
+            PlayerProfileService profileService = Container.Resolve<PlayerProfileService>();
+            profileService.SetTotalLives(3);
+            int totalLives = profileService.GetTotalLives();
 
-        var profileService = Container.Resolve<PlayerProfileService>();
-        profileService.SetTotalLives(3);
-        var totallives = profileService.GetTotalLives();
+            PlayerShip ship = Container.Resolve<PlayerShip>();
+            Bullet bullet = Container.Resolve<Bullet>();
+            bullet.SetVisibility(true);
 
-        var ship = Container.Resolve<PlayerShip>();
-        var bullet = Container.Resolve<Bullet>();
-        bullet.SetVisibility(true);
+            ship.transform.position = bullet.transform.position = new Vector3(0, 0, 0);
 
-        ship.transform.position = bullet.transform.position = new Vector3(0, 0, 0);
+            yield return null;
 
-        yield return null;
+            Assert.Less(profileService.GetTotalLives(), totalLives);
+        }
 
-        Assert.Less(profileService.GetTotalLives(), totallives);
-    }
+        private void CommonPlayerShipInstallBindings<TComponent>(GameConfig gameConfig, GameObject mockGameObject)
+        {
+            Camera cam = new GameObject("Main Camera").AddComponent<Camera>();
+            cam.tag = "MainCamera";
 
-    private void CommonPlayerShipInstallBindings<TComponent>(GameConfig gameConfig, GameObject mockGameObject)
-    {
-        var cam = new GameObject("Main Camera").AddComponent<Camera>();
-        cam.tag = "MainCamera";
+            PreInstall();
 
-        PreInstall();
+            // Call Container.Bind methods
+            Container.Bind<MainManager>().FromNewComponentOnNewGameObject().AsSingle();
+            Container.Bind<PlayerShip>().FromComponentInNewPrefab(gameConfig.PlayerShip).AsSingle();
+            Container.Bind<TComponent>().FromComponentInNewPrefab(mockGameObject).AsSingle();
 
-        // Call Container.Bind methods
+            Container.BindFactory<Object, GameEntity, GameEntity.Factory>()
+                .FromFactory<PrefabFactory<GameEntity>>().WhenInjectedInto<GameEntitySpawnController>();
 
-        Container.Bind<MainManager>().FromNewComponentOnNewGameObject().AsSingle();
-        Container.Bind<PlayerShip>().FromComponentInNewPrefab(gameConfig.PlayerShip).AsSingle();
-        Container.Bind<TComponent>().FromComponentInNewPrefab(mockGameObject).AsSingle();
+            Container.BindInterfacesAndSelfTo<SignalService>().AsSingle();
+            Container.BindInterfacesAndSelfTo<AssetProvider>().AsSingle();
+            Container.BindInterfacesAndSelfTo<GameContainer>().AsSingle();
+            Container.BindInterfacesAndSelfTo<EnemySpawnController>().AsSingle();
+            Container.BindInterfacesAndSelfTo<GameEntitySpawnController>().AsSingle();
+            Container.BindInterfacesAndSelfTo<ConfigCollectionService>().AsSingle();
+            Container.BindInterfacesAndSelfTo<PlayerProfileService>().AsSingle();
+            Container.BindInterfacesAndSelfTo<GameStateManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<GameLoop>().AsSingle().WhenInjectedInto<GameContainer>();
 
-        Container.BindFactory<Object, GameEntity, GameEntity.Factory>()
-            .FromFactory<PrefabFactory<GameEntity>>().WhenInjectedInto<GameEntitySpawnController>();
+            Container.Bind<UiManager>().AsSingle();
 
-        Container.BindInterfacesAndSelfTo<SignalService>().AsSingle();
-        Container.BindInterfacesAndSelfTo<AssetProvider>().AsSingle();
-        Container.BindInterfacesAndSelfTo<GameContainer>().AsSingle();
-        Container.BindInterfacesAndSelfTo<EnemySpawnController>().AsSingle();
-        Container.BindInterfacesAndSelfTo<GameEntitySpawnController>().AsSingle();
-        Container.BindInterfacesAndSelfTo<ConfigCollectionService>().AsSingle();
-        Container.BindInterfacesAndSelfTo<PlayerProfileService>().AsSingle();
-        Container.BindInterfacesAndSelfTo<GameLoop>().AsSingle().WhenInjectedInto<GameContainer>();
+            PostInstall();
 
-        PostInstall();
-        Container.Resolve<MainManager>();
+            Container.Resolve<MainManager>();
+        }
     }
 }
